521dd36ea7792bef0b487691ad35006bd8290ac4
Zachary <128825060+zachary-caudle@users.noreply.github.com>
2024-02-14 08:49:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+//I'm using this awesome Css Gradient Pattern from Lea Verou's Gallery: http://lea.verou.me/css3patterns/#hearts
+
+//Also Here is my previous Valentine's pen: https://codepen.io/AngelaVelasquez/details/KqCDz/
